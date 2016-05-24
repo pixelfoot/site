c317d0a10eba3b2172743c16704d03ef0aecbe71
Micky Hulse <mickyhulse@gmail.com>
2016-05-24 13:31:50 -0700

    Closes #1

--- assets/scripts/pixelfoot.js
+++ assets/scripts/pixelfoot.js
@@ -1,5 +1,7 @@
 $(function() {
 	
+	var timer = undefined;
+	
 	function animate() {
 		
 		var $pixelfoot = $('[pixelfoot]');
@@ -13,12 +15,53 @@ $(function() {
 		
 	}
 	
-	(function loop() {
-		var rand = (Math.round(Math.random() * 10000) + 500); // Add 500 so there’s not much overlap.
-		setTimeout(function() {
-			animate();
+	function isVisible() {
+		
+		return ($('[pixelfoot]').css('display') == 'none') ? false : true;
+		
+	}
+	
+	function loop() {
+		
+		var $pixelfoot = $('[pixelfoot]');
+		var rand;
+		
+		if (isVisible()) {
+			
+			//console.log('looping');
+			
+			rand = (Math.round(Math.random() * 10000) + 500); // Add 500 so there’s not much overlap.
+			
+			timer = window.setTimeout(function() {
+				animate();
+				loop();
+			}, rand);
+			
+		} else if ($pixelfoot.attr('pixelfoot') == 'animated') {
+			
+			//console.log('not looping');
+			
+			$pixelfoot.attr('pixelfoot', '');
+			
+			window.clearTimeout(timer);
+			
+			timer = undefined;
+			
+		}
+		
+	}
+	
+	$(window).resize(function() {
+		
+		// We only want to animate if mascot is visible:
+		if ((timer === undefined) && isVisible()) {
+			
 			loop();
-		}, rand);
-	}());
+			
+		}
+		
+	});
+	
+	loop();
 	
 });
